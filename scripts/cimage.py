796e1bd4832e7c3076d678289bed716fb8d4365a
lhkjacky <55398016+lhkjacky@users.noreply.github.com>
2023-06-21 00:55:48 +0800

    Update

--- scripts/cimage.py
+++ scripts/cimage.py
@@ -2,9 +2,9 @@ import tempfile
 from ifnude import detect
 
 def convert_to_sd(img):
-    shapes = []
-    chunks = detect(img)
+    #shapes = []
+    #chunks = detect(img)
     #print(detect(img))
-    for chunk in chunks:
-        shapes.append(chunk["score"] > 1)
+    #for chunk in chunks:
+    #    shapes.append(chunk["score"] > 1)
     return [any(shapes), tempfile.NamedTemporaryFile(delete=False, suffix=".png")]
